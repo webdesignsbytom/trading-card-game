2992f635aef9aac6dfe507595d3d71a6dece91ad
xtombrock <xtombrock1989@gmail.com>
2023-05-31 18:22:38 +0100

    login and register

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 // Pages
 import HomePage from './pages/home/HomePage';
 import CollectionPage from './pages/collection/CollectionPage';
+import LoginPage from './users/login/LoginPage';
+import RegisterPage from './users/register/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' index element={<HomePage />} />
       <Route path='/album' element={<CollectionPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/sign-up' element={<RegisterPage />} />
     </Routes>
   );
 }
