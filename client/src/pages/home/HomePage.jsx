7bbc97bc85883567858e2b7360d7da7a7a00c8d6
Thomas Brockington <xtombrock1989@gmail.com>
2023-06-03 21:59:18 +0100

    working data gathering

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div className='bg-black'>HomePage</div>
+    <div className='bg-black'>
+      <img src="https://www.parallelparliament.co.uk/static/UK_Parliament/img/mp_images/4057-15x15.jpeg" alt="parlament" />
+    </div>
   )
 }
 
--- webscrapper/WebScrapper.cs
+++ webscrapper/WebScrapper.cs
@@ -1,40 +1,77 @@
 using HtmlAgilityPack;
-using System;
-using System.Net.Http;
+using System.Xml.Linq;
 
-namespace WebScrapper
+// This progream collects a list of people from a website
+// Narrows down to get each members name and an image url
+// The adds items to a Dictionary
+namespace WebScrapper2
 {
      class Program
     {
-        // Main method to run program
         static void Main(string[] args)
         {
-            // Where to get data from
-            String url = "https://weather.com/en-GB/weather/today/l/UKXX0085:1:UK";
 
+            // Where to get data from
+            String url = "https://www.parallelparliament.co.uk/MPs";
+            String srcUrl = "https://www.parallelparliament.co.uk";
+
+            int startingIndex = 1;
             // Create a client object
             var httpClient = new HttpClient();
+
             // use get string to create a get request
             // .result will give us a field
             var html = httpClient.GetStringAsync(url).Result;
-            // Console.WriteLine(html); // This will display all html
+            // Console.WriteLine("HTML: " + html); // This will display all html
 
             // Convert to a format
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(html);
 
-            // Get temperature
-            // Get all elements in the html script from our document that match our needs
-            var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='CurrentConditions--tempValue--MHmYY']");
-            Console.WriteLine("temp e = " + temperatureElement);
-            var temperature = temperatureElement.InnerText.Trim();
-            Console.WriteLine("temp inner = " + temperature);
-            // Get conditions
+            HtmlNode[] nodes = htmlDocument.DocumentNode.SelectNodes("/html/body/div[3]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]").ToArray();
 
-            // Get location
+            List<string> nameList = new List<string>();
 
-            // Console.WriteLine(htmlDocument);
+            Dictionary<int, MemberCard> cards = new Dictionary<int, MemberCard>();
+
+            foreach (HtmlNode item in nodes)
+            {
+                Console.WriteLine("ITEM" + item.InnerHtml);
+
+                var img = item.SelectSingleNode("a/img");
+                Console.WriteLine(img.Attributes["src"].Value);
+
+                var src = img.Attributes["src"].Value;
+                Console.WriteLine("IMage src" + src);
+
+                var fullSrcUrl = srcUrl + src;
+                Console.WriteLine("Full " + fullSrcUrl);
+
+                // Get name
+                var _name = item.SelectSingleNode("div/a/h3");
+                var nome = _name.InnerText;
+                Console.WriteLine($"_NAME {_name}");
+                Console.WriteLine($"NOME {nome}");
+
+                cards.Add(startingIndex, new MemberCard() { Name = nome, ImageURL = fullSrcUrl });
+                startingIndex++;
+            }
+
+            foreach (MemberCard card in cards.Values)
+            {
+                Console.WriteLine("FOUND " + card.Name);
+                Console.WriteLine("FOUND " + card.ImageURL);
+            }
+
             Console.ReadLine();
         }
     }
+
+    class MemberCard
+    {
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+
+    }
+    
 }
