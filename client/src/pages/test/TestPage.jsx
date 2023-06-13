d1def7b3367e93d711a7a7f06de52608f87849f0
xtombrock <xtombrock1989@gmail.com>
2023-06-13 15:21:11 +0100

    save

--- client/src/pages/test/TestPage.jsx
+++ client/src/pages/test/TestPage.jsx
@@ -1,42 +1,67 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Card from '../../components/card/Card';
+import client from '../../utils/client';
 function TestPage() {
+  const [cardData, setCardData] = useState({});
+
+  useEffect(() => {
+    client
+      .get(`/con-cards/card/get-by-id/1`)
+      .then((res) => {
+        setCardData(res.data.data.card);
+      })
+      .catch((err) => {
+        console.error('Unable to find card', err);
+      });
+  }, []);
+
   return (
     <div className='grid h-screen bg-slate-200'>
       <main className='grid items-center px-4 grid-rows-2'>
         <section className='grid grid-cols-5 gap-4 w-full'>
           <div className='grid outline-2 outline outline-black rounded-xl h-full w-full bg-holo-bg bg-repeat bg-center bg-50'>
-            a
+            1
           </div>
           <div className='grid outline-2 outline outline-black rounded-xl h-full w-full rainbowGradientOne'>
-            a
+            2
           </div>
           <div className='grid outline-2 outline outline-black rounded-xl h-full w-full rainbowGradientTwo'>
-            a
+            3
           </div>
           <div className='grid outline-2 outline outline-black rounded-xl h-full w-full radialGradient'>
-            a
+            4
           </div>
           <div className='grid outline-2 outline outline-black rounded-xl h-full w-full rainbowsCombined'>
-            a
+            5
           </div>
         </section>
 
         <section className='grid grid-cols-5 gap-4 w-full'>
-          <div className='grid outline-2 outline outline-black rounded-xl h-full w-full allGradientsCombined'>
-            a
-          </div>
           <div className='grid outline-2 outline outline-black rounded-xl h-full w-full textureAndAllGradientsCombined'>
-            a
+            6
           </div>
           <div className='grid outline-2 outline outline-black rounded-xl h-full w-full allWithBackgroundPosition'>
-            a
+            7
           </div>
           <div className='grid outline-2 outline outline-black rounded-xl h-full w-full cardShinePlusAfterElement'>
-            a
+            8
           </div>
-          <div className='grid outline-2 outline outline-black rounded-xl h-full w-full rainbowGradientOne'>
-            a
+          <div className='card'>
+            <div className='cardFront'>
+              <img
+                src='https://images.pokemontcg.io/swsh1/190_hires.png'
+                alt='name'
+              />
+              <div className='cardShinePlusAfterElement'></div>
+              <div className='cardGlare'></div>
+            </div>
+          </div>
+          <div className='grid outline-2 outline outline-black rounded-xl h-full w-full'>
+            <img
+              className='h-full cardShinePlusAfterElement'
+              src='https://images.pokemontcg.io/swsh1/190_hires.png'
+              alt='test'
+            />
           </div>
         </section>
       </main>
